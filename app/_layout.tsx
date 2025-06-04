d925737799a3c20de2aea10f6252515d4e57279a
zohaibhammad <zohaibhammad@gmail.com>
2025-06-04 20:21:26 +1000

    Wrapped the app with the Task repository provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
+import { TaskRepositoryProvider } from "../repositories/TaskRepository";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+      <TaskRepositoryProvider>
+        <Stack />
+      </TaskRepositoryProvider>
+  );
 }
