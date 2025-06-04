e05fbdd1aa2f17689cdfeb91eb22dfb046a712e5
zohaibhammad <zohaibhammad@gmail.com>
2025-06-04 20:21:53 +1000

    Created a task listing page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { View, Text, Button, FlatList } from "react-native";
+import { useRouter } from "expo-router";
+import {Task, useTaskRepository} from "@/repositories/TaskRepository";
+import { useEffect, useState } from "react";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+export default function HomeScreen() {
+    const router = useRouter();
+    const { getTasks } = useTaskRepository();
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    useEffect(() => {
+        setTasks(getTasks());
+        console.log("Hello")
+    }, []);
+
+    return (
+        <View style={{ flex: 1, padding: 20 }}>
+            <Button title="Add Task" onPress={() => router.push("/task-form")} />
+            <FlatList
+                data={tasks}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <Text style={{ marginTop: 10 }}>{item.title}</Text>
+                )}
+            />
+        </View>
+    );
 }
