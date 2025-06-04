6dbca129ea03fdb886ca4c00a0fc859c92a0f56b
zohaibhammad <zohaibhammad@gmail.com>
2025-06-04 20:20:46 +1000

    Created a Context API based task repository

---
+++ repositories/TaskRepository.tsx
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useState } from "react";
+
+export type Task = {
+    id: number;
+    title: string;
+};
+
+type TaskContextType = {
+    tasks: Task[];
+    getTasks: () => Task[];
+    addTask: (task: Task) => void;
+    deleteTask: (id: number) => void;
+};
+
+const TaskContext = createContext<TaskContextType | undefined>(undefined);
+
+export const TaskRepositoryProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    const getTasks = () => tasks;
+    const addTask = (task: Task) => setTasks((prev) => [...prev, task]);
+    const deleteTask = (id: number) => setTasks((prev) => prev.filter((t) => t.id !== id));
+
+    return (
+        <TaskContext.Provider value={{ tasks, getTasks, addTask, deleteTask }}>
+            {children}
+        </TaskContext.Provider>
+    );
+};
+
+export const useTaskRepository = () => {
+    const context = useContext(TaskContext);
+    if (!context) throw new Error("useTaskRepository must be used within a TaskRepositoryProvider");
+    return context;
+};
